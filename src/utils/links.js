4bc52d1568be348d3476557a543f9457d25718a5
igor-chazov <igor-chazov@yandex.ru>
2022-02-14 22:04:16 +0500

    Исправлена ссылка

--- src/utils/links.js
+++ src/utils/links.js
@@ -5,7 +5,7 @@ if (url.hostname === 'localhost') {
 }
 
 if (url.hostname === 'igor-chazov.github.io') {
-  url.hostname = 'ra-12-2-backend1.herokuapp.com';
+  url.hostname = 'ra-12-3-backend1.herokuapp.com';
   url.protocol = 'https';
 }
 
